874e1124cd6fa9d4225f3be1353f147b9e0d2dc4
anjana732 <anjanasingh1257@gmail.com>
2024-08-04 23:08:12 +0530

    add lead source code

--- src/components/LeadSource.js
+++ src/components/LeadSource.js
@@ -1,11 +1,9 @@
-// src/components/LeadSource.js
-
 import React from 'react';
 import { Pie } from 'react-chartjs-2';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'; // Import necessary components
-//import './LeadSource.css'; // Import the CSS file
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 
-ChartJS.register(ArcElement, Tooltip, Legend); // Register components
+
+ChartJS.register(ArcElement, Tooltip, Legend); 
 
 const LeadSource = () => {
   // Example data for the chart
